247160194a82f1212b72be562edc7c6f6e5549d4
Tatyana Amosova <atgatg@bk.ru>
2024-01-14 18:30:36 +0300

    /dist corrected

--- src/js/__tests__/Character.test.js
+++
@@ -1,8 +0,0 @@
-import Character from '../Character';
-
-test('testing that function throws', () => {
-  expect(() => {
-    const character = new Character(1, 'bowman');
-    return character;
-  }).toThrow('Нельзя создавать объект класса Character!');
-});
--- src/js/characters/Magician.js
+++
@@ -1,9 +0,0 @@
-import Character from '../Character';
-
-export default class Magician extends Character {
-  constructor(level, type = 'magician') {
-    super(level, type);
-    this.attack = 30;
-    this.defence = 40;
-  }
-}
